f07a1e40833d65909960fe47846c1d6421ff6b3d
Sherzodjon <124202641+tursunov4@users.noreply.github.com>
2025-05-31 00:05:33 +0500

    dasda

--- src/components/LightingPage/LightingPage.vue
+++ src/components/LightingPage/LightingPage.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>lighting</div>
+  <div class="desktop:block hidden">
+    <div
+      class="flex flex-col items-center desktop:mt-[155px] mt-[120px] mb-[50px]"
+    >
+      <div
+        class="flex flex-col uppercase items-center gap-[14px] desktop:gap-[30px] mx-auto my-auto"
+      >
+        <h1
+          class="title text-center desktop:text-[40px] desktop:leading-[10px] text-[32px] leading-[10px]"
+        >
+          Освещение
+        </h1>
+        <div
+          class="desktop:text-[18px] min-h-[27px] text-[10px] font-TT font-bold text-center"
+        >
+          Подкатегории
+        </div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="desktop:block hidden desktop:mb-[20px] mb-[10px] text-[14px] text-end uppercase px-[20px] flex desktop:justify-end align-center gap-3"
+  >
+    <RouterLink class="text-customGray" to="/"> ГЛАВНАЯ </RouterLink>
+    - <RouterLink to="#">Освещение</RouterLink>
+  </div>
+  <LightingSubcategories />
+
+  <LighthingNewProducts />
+  <div class="desktop:aspect-video aspect-[4/5]"><AboutVideo /></div>
 </template>
+
+<script setup lang="ts">
+import AboutVideo from "../HomePage/AboutVideo.vue";
+import LighthingNewProducts from "./LighthingNewProducts.vue";
+
+import LightingSubcategories from "./LightingSubcategories.vue";
+</script>
 
 <style lang="css" scoped></style>
---
+++ src/components/LightingPage/LightingSubcategories.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="border-t border-l mx-5 border-[#101010] grid grid-cols-1 desktop:grid-cols-2"
+  >
+    <div
+      v-for="item in items"
+      :key="item.title"
+      class="border-b p-[30px] border-r border-[#101010] flex flex-col"
+    >
+      <!-- Image -->
+      <div
+        class="w-full h-[200px] desktop:h-[340px] bg-cover bg-center"
+        :style="{ backgroundImage: `url(${item.image || defaultImage})` }"
+      ></div>
+
+      <!-- Title + Count -->
+      <div class="flex justify-between items-center mt-5 uppercase">
+        <span class="text-[14px] font-bold desktop:text-[18px]">{{
+          item.title
+        }}</span>
+        <span class="text-[10px] desktop:text-[12px]">{{ item.count }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const defaultImage = new URL("../../assets/mainPage/doors.png", import.meta.url)
+  .href;
+
+const items = [
+  {
+    title: "Диммеры",
+    count: 34,
+    image: "", // fallback image
+  },
+  {
+    title: "Розетки",
+    count: 45,
+    image: "",
+  },
+  {
+    title: "Выключатели",
+    count: 8,
+    image: "",
+  },
+  {
+    title: "Тёплый пол",
+    count: 120,
+    image: "",
+  },
+  {
+    title: "Потолочные светильники",
+    count: 20,
+    image: "",
+  },
+];
+</script>
