87e03205193f753cad0433f484249d4a6bdcae02
Sherzodjon <124202641+tursunov4@users.noreply.github.com>
2025-06-14 01:09:02 +0500

    dsaa

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
-import {onMounted} from "vue";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { onMounted } from "vue";
 
 let map;
 let Icon = L.icon({
-  iconUrl: '/logo.svg',
-  iconSize:[30, 30],
+  iconUrl: "/logo.svg",
+  iconSize: [30, 30],
 });
 onMounted(() => {
-  map = L.map('map',{ attributionControl:false, zoomControl:false}).setView([55.83676, 37.44891], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/adminroman/clmurpl7r02k601qu2ne7g93e/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWRtaW5yb21hbiIsImEiOiJjbG11ajlhYWQwY2xpMmtxcHNqM3BoNzB0In0.p2XIJ6HLczRQyQWUotyCZw', {
-    maxZoom: 19,
-  }).addTo(map);
-  L.marker([55.837449, 37.446623], {icon: Icon}).bindPopup('<h1>DEPP DESIGN</h1><p>Москва, Ул Свободы 35 стр. 17').openPopup().addTo(map);
-})
-
+  map = L.map("map", { attributionControl: false, zoomControl: false }).setView(
+    [55.83676, 37.44891],
+    13
+  );
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/adminroman/clmurpl7r02k601qu2ne7g93e/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWRtaW5yb21hbiIsImEiOiJjbG11ajlhYWQwY2xpMmtxcHNqM3BoNzB0In0.p2XIJ6HLczRQyQWUotyCZw",
+    {
+      maxZoom: 19,
+    }
+  ).addTo(map);
+  L.marker([55.837449, 37.446623], { icon: Icon })
+    .bindPopup("<h1>DEPP DESIGN</h1><p>Москва, Ул Свободы 35 стр. 17")
+    .openPopup()
+    .addTo(map);
+});
 </script>
 <template>
   <div id="map" class="w-full relative z-0 h-full"></div>
@@ -37,7 +45,7 @@ onMounted(() => {
   border-radius: 0;
 }
 
-.leaflet-popup-tip{
+.leaflet-popup-tip {
   background-color: #101010;
 }
 
@@ -52,8 +60,4 @@ onMounted(() => {
 .leaflet-popup-content-wrapper {
   border-radius: 0;
 }
-
-.leaflet-popup-content-wrapper .leaflet-popup-content {
-}
-
 </style>
--- src/components/PhotoCarousel.vue
+++ src/components/PhotoCarousel.vue
@@ -1,87 +1,91 @@
 <script setup lang="ts">
-import {Swiper, SwiperSlide} from 'swiper/vue';
-import 'swiper/css';
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
 
-import "swiper/css/free-mode"
-import "swiper/css/thumbs"
-import {Navigation, Thumbs} from 'swiper/modules';
-import {ref, watch, onUnmounted} from "vue";
-import {components} from "../types/schema";
-import {useI18n} from "vue-i18n";
+import "swiper/css/free-mode";
+import "swiper/css/thumbs";
+import { Navigation, Thumbs } from "swiper/modules";
+import { ref, watch, onUnmounted } from "vue";
+import { components } from "../types/schema";
+import { useI18n } from "vue-i18n";
 
-const {t} = useI18n({useScope: 'global'})
-const modules = [Navigation, Thumbs]
+const { t } = useI18n({ useScope: "global" });
+const modules = [Navigation, Thumbs];
 
-const selectedImage = ref(0)
+const selectedImage = ref(0);
 
 let thumbsSwiper = ref(null);
-let zoomSwiper = ref(null)
-let showZoom = ref(false)
+let zoomSwiper = ref(null);
+let showZoom = ref(false);
 
 const setThumbsSwiper = (swiper: any) => {
   thumbsSwiper.value = swiper;
-  zoomSwiper.value = swiper
-}
+  zoomSwiper.value = swiper;
+};
 
 defineProps<{
-  images: components["schemas"]["Image"][]
-}>()
+  images: components["schemas"]["Image"][];
+}>();
 let scrollTop: number;
 watch(showZoom, () => {
   if (showZoom.value) {
-    let body = document.querySelector('body') as HTMLBodyElement | null
-    let html = document.querySelector('html') as HTMLBodyElement | null
+    let body = document.querySelector("body") as HTMLBodyElement | null;
+    let html = document.querySelector("html") as HTMLBodyElement | null;
     if (body && html) {
       const b = document.body;
-      b.classList.add('noscroll');
+      b.classList.add("noscroll");
     }
   } else {
-    let body = document.querySelector('body') as HTMLBodyElement | null
-    let html = document.querySelector('html') as HTMLBodyElement | null
+    let body = document.querySelector("body") as HTMLBodyElement | null;
+    let html = document.querySelector("html") as HTMLBodyElement | null;
     if (body && html) {
       const b = document.body;
-      b.classList.remove('noscroll');
+      b.classList.remove("noscroll");
       html.scrollTo(0, scrollTop);
     }
   }
-})
+});
 onUnmounted(() => {
-  let body = document.querySelector('body') as HTMLBodyElement | null
-  let html = document.querySelector('html') as HTMLBodyElement | null
+  let body = document.querySelector("body") as HTMLBodyElement | null;
+  let html = document.querySelector("html") as HTMLBodyElement | null;
   if (body && html) {
     const b = document.body;
-    b.classList.remove('noscroll');
+    b.classList.remove("noscroll");
     html.scrollTo(0, scrollTop);
   }
-})
-
+});
 </script>
 
 <template>
-  <div v-if="showZoom" class="absolute z-20 w-full flex flex-col bg-white mt-[-220px] right-0">
+  <div
+    v-if="showZoom"
+    class="absolute z-20 w-full flex flex-col bg-white mt-[-220px] right-0"
+  >
     <div class="h-[100vh]">
-      <div class="font-TT text-[1vw] font-bold flex justify-center items-center mt-[3vh]">
+      <div
+        class="font-TT text-[1vw] font-bold flex justify-center items-center mt-[3vh]"
+      >
         <div class="mb-[3vh]">
-          {{t('product_page.zoom').toUpperCase()}}
+          {{ t("product_page.zoom").toUpperCase() }}
         </div>
-        <div @click="showZoom=false" class="close absolute right-0 mt-[-3.7vh] mr-[3vh]"></div>
+        <div
+          @click="showZoom = false"
+          class="close absolute right-0 mt-[-3.7vh] mr-[3vh]"
+        ></div>
       </div>
       <div class="w-full h-[88vh]">
         <swiper
-            :loop="true"
-            :slidesPerView="'auto'"
-            :modules="modules"
-            :direction="'horizontal'"
-            :navigation="true"
-            :initial-slide="selectedImage"
-            class="zoomSwiper w-full h-full relative first overflow-x-visible"
+          :loop="true"
+          :slidesPerView="'auto'"
+          :modules="modules"
+          :direction="'horizontal'"
+          :navigation="true"
+          :initial-slide="selectedImage"
+          class="zoomSwiper w-full h-full relative first overflow-x-visible"
         >
           <swiper-slide v-for="image in images"
-          ><img
-                class="h-full aspect-[64/73] mx-auto"
-                :src="image.photo"/>
-          </swiper-slide
-          >
+            ><img class="h-full mx-auto" :src="image.photo" />
+          </swiper-slide>
         </swiper>
       </div>
     </div>
@@ -90,50 +94,51 @@ onUnmounted(() => {
   <div class="w-full h-full flex justify-between gap-[1.65vw] -ml-[5px]">
     <div class="-mt-[4px] overflow-x-visible">
       <swiper
-          @swiper="setThumbsSwiper"
-          :loop="false"
-          :slidesPerView="'auto'"
-          :modules="modules"
-          :direction="'vertical'"
-          :navigation="true"
-          class="photoSwiper relative first overflow-x-visible"
+        @swiper="setThumbsSwiper"
+        :loop="false"
+        :slidesPerView="'auto'"
+        :modules="modules"
+        :direction="'vertical'"
+        :navigation="true"
+        class="photoSwiper relative first overflow-x-visible"
       >
-        <swiper-slide v-for="(image, index) in images" @click="selectedImage = index"
-        ><img :class="{'border-black border-[1px]' : selectedImage == index}"
-              class="object-cover box-border min-h-[84px] p-[4px] h-[84px] min-w-[74px] max-w-[74px] w-[74px] "
-              :src="image.photo"/>
-        </swiper-slide
-        >
+        <swiper-slide
+          v-for="(image, index) in images"
+          @click="selectedImage = index"
+          ><img
+            :class="{ 'border-black border-[1px]': selectedImage == index }"
+            class="object-cover box-border min-h-[84px] p-[4px] h-[84px] min-w-[74px] max-w-[74px] w-[74px]"
+            :src="image.photo"
+          />
+        </swiper-slide>
       </swiper>
     </div>
     <swiper
-        :style="{
-      '--swiper-navigation-color': '#fff',
-      '--swiper-pagination-color': '#fff',
-    }"
-        :allowTouchMove="false"
-        :modules="modules"
-        :direction="'vertical'"
-        space-between="10px"
-        :loop="true"
-        :thumbs="{ swiper: thumbsSwiper }"
-        class="w-full aspect-[64/73] vertical"
+      :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+      }"
+      :allowTouchMove="false"
+      :modules="modules"
+      :direction="'vertical'"
+      space-between="10px"
+      :loop="true"
+      :thumbs="{ swiper: thumbsSwiper }"
+      class="w-full vertical"
     >
-      <swiper-slide v-for="image in images"
-
-      >
-        <div @click="showZoom=true" class="overflow-hidden hover:cursor-crosshair w-full h-full"><img
-            class="object-cover h-full w-full"
-            :src="image.photo"/>
+      <swiper-slide v-for="image in images">
+        <div
+          @click="showZoom = true"
+          class="overflow-hidden hover:cursor-crosshair w-full h-full"
+        >
+          <img class="object-cover h-full w-full" :src="image.photo" />
         </div>
-      </swiper-slide
-      >
+      </swiper-slide>
     </swiper>
   </div>
 </template>
 
 <style>
-
 .first .swiper-slide {
   margin: 0 !important;
 }
@@ -152,7 +157,7 @@ onUnmounted(() => {
   --swiper-navigation-color: black;
 }
 
-.zoomSwiper > .swiper-button-next{
+.zoomSwiper > .swiper-button-next {
   right: 0;
 }
 
@@ -188,7 +193,8 @@ onUnmounted(() => {
   display: none !important;
 }
 
-.zoomSwiper > .swiper-button-prev, .zoomSwiper > .swiper-button-next {
+.zoomSwiper > .swiper-button-prev,
+.zoomSwiper > .swiper-button-next {
   top: calc(50% - 45px);
   z-index: 999;
 }
@@ -223,7 +229,7 @@ onUnmounted(() => {
 
 .zoomSwiper > .swiper-button-prev:after,
 .zoomSwiper > .swiper-rtl .swiper-button-next:after {
-  content: 'prev';
+  content: "prev";
 }
 
 .zoomSwiper > .swiper-button-next,
@@ -233,7 +239,6 @@ onUnmounted(() => {
 
 .zoomSwiper > .swiper-button-next:after,
 .zoomSwiper > .swiper-rtl .swiper-button-prev:after {
-  content: 'next';
+  content: "next";
 }
-
 </style>
